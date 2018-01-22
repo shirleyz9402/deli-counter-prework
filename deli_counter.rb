4650804f4ea56f5b67aed7a8a76e0ef349f43914
Shirley Zhang <shirleyz9402@yahoo.com>
2018-01-22 20:36:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,9 @@
 def line(katz_deli)
   theline = []
   katz_deli.each_with_index{|katz_deli,index| theline.push("#{index + 1}. #{katz_deli}")}
-  return "The line is currently: #{theline.join(" ")}"
+  if theline.length < 1 
+    return "The line is currently empty."
+  else 
+    return "The line is currently: #{theline.join(" ")}"
+  end 
 end 
