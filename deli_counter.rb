e6255d3936f9fbf787a700330ce1d555ea0c7e80
Shirley Zhang <shirleyz9402@yahoo.com>
2018-01-22 20:40:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,6 +7,6 @@ def line(katz_deli)
     puts "The line is currently: #{theline.join(" ")}"
   end 
 end 
-def take_a_number(katz_deli,name)
+def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length + 1} in line."
 end 
