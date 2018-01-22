a4c04f319703588d331aadc74cd8f7438c4afb19
Shirley Zhang <shirleyz9402@yahoo.com>
2018-01-22 20:35:09 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,5 @@
 def line(katz_deli)
   theline = []
-  katz_deli.each_with_index{|katz_deli,index| 
+  katz_deli.each_with_index{|katz_deli,index| theline.push("#{index + 1}. #{katz_deli}")}
+  return "The line is currently: #{theline.join(" ")}"
+end 
